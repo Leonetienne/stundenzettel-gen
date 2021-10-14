8950e8e5a1b29aa400bfd478f665a4d38bfd5393
Leon Etienne <etienne@medienagenten.de>
2021-10-14 12:46:31 +0200

    fixed übernahemdaten

--- auto_generate.sh
+++ auto_generate.sh
@@ -1,2 +1,3 @@
 # Will call generate with the correct values for this week
+echo "Generating time sheet for calendar week: " `date -v -Thur +%V`
 ./generate.sh `date +%V` `date -v -Wed +%d.%m.%Y` `date -v -Thur +%d.%m.%Y`
